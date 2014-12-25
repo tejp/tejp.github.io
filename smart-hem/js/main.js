db7fac3196f35a1e7e78567654c5b4b5b52716f7
tejp <cyberkygen@gmail.com>
2014-12-25 06:32:03 +0100

    google analytics

--- smart-hem/js/main.js
+++ smart-hem/js/main.js
@@ -1,11 +1,66 @@
-$(document).ready(function(){
+function loadMain (i) {
+	$('#view').empty();
+	var divRow = $('<div />', { 'class': 'row view-main' });
+	var divNav = $('<div />', { 'class': 'small-12 medium-7 column' });
+	var divOpt = $('<div />', { 'class': 'small-12 medium-5 column' });
+	var nav = loadNavigation(i);
+	var opt = loadOptions(i);
 
-$(".house-livingroom").click(function(){
-	settings.currentPage = 'view-livingroom'; loadSettings(); });
-$(".house-kitchen").click(function(){
-	settings.currentPage = 'view-kitchen'; loadSettings(); });
-$(".house-bedroom").click(function(){
-	settings.currentPage = 'view-bedroom'; loadSettings(); });
-$(".house-wc").click(function(){
-	settings.currentPage = 'view-wc'; alert(settings.currentPage); loadSettings(); });
-});
+	divRow.append(
+		divNav.append(nav), divOpt.append(opt)
+	).appendTo('#view');
+}
+
+function loadNavigation (i) {
+	switch (i) {
+		case 'view-house':
+			return loadHouse();
+			break;
+		default:
+			return loadDefault(i);
+	}
+}
+
+function loadHouse () {
+	var ssr = $('<div />', { 'class': 'static-size-ratio ssr-80'});
+	var ssrC = $('<div />', { 'class': 'ssr-content'});
+	var house = $('<div />', { 'class': 'view-house'});
+	var houseFill = ['wc', 'bedroom', 'kitchen', 'balcony', 'livingroom'];
+	$.each(houseFill, function (i, o) {
+		var room = $('<div />', { 'class': 'center-child house-' + o });
+
+		if(o !== 'balcony'){
+			room.click(function(){ loadMain('view-' + o); });
+		}
+		room.append(o).appendTo(house);
+	});
+
+	return ssr.append(ssrC.append(house));
+}
+
+function loadDefault (i) {
+	var ssr = $('<div />', { 'class': 'static-size-ratio ssr-80'});
+	var ssrC = $('<div />', { 'class': 'ssr-content center-child'});
+	var room = $('<div />', { 'class': 'center-child room-' + i.substr(5) });
+
+	return ssr.append(ssrC.append(room.append(i.substr(5))));
+}
+
+function loadOptions (j) {
+	var ul = $('<ul />', { 'class': 'no-bullet options-list'});
+
+	$.each(settings[j], function(i, o){
+		var li = $('<li />', { 'class': 'options-list-item' });
+		var div = $('<div />', { 'class': 'switch tiny' });
+		var input = $('<input />', {
+			'type': 'checkbox',
+			'id': i,
+			'checked': o.value === 1 }).click(function () {
+				settings[j][this.id]["value"] = 
+				+(this.checked);
+			});
+		var label = $('<label />', { 'for': i });
+		li.append(i, div.append(input, label)).appendTo(ul);
+	});
+	return ul;
+}
