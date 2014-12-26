f8f34182f2649cbd40b7630c05928b7fdc92dbb2
tejp <cyberkygen@gmail.com>
2014-12-26 05:27:58 +0100

    comment google analytics

--- smart-hem/js/main.js
+++ smart-hem/js/main.js
@@ -1,23 +1,29 @@
-function loadMain (i) {
+function navigateMain (page) {
+	loadMain(page); $(document).foundation('reflow'); }
+
+function loadMain (page) {
 	$('#view').empty();
 	var divRow = $('<div />', { 'class': 'row view-main' });
 	var divNav = $('<div />', { 'class': 'small-12 medium-7 column' });
 	var divOpt = $('<div />', { 'class': 'small-12 medium-5 column' });
-	var nav = loadNavigation(i);
-	var opt = loadOptions(i);
+	var nav = loadNavigation(page);
+	var opt = loadOptions(page);
 
 	divRow.append(
 		divNav.append(nav), divOpt.append(opt)
 	).appendTo('#view');
 }
 
-function loadNavigation (i) {
-	switch (i) {
-		case 'view-house':
-			return loadHouse();
-			break;
-		default:
-			return loadDefault(i);
+function loadNavigation (page) {
+	switch (page) {
+	case 'view-house':
+		return loadHouse();
+		break;
+	default:
+		var ssr = $('<div />', { 'class': 'static-size-ratio ssr-80'});
+		var ssrC = $('<div />', { 'class': 'ssr-content center-child'});
+		var room = $('<div />', { 'class': 'center-child room-' + page.substr(5) });
+		return ssr.append(ssrC.append(room.append(page.substr(5))));
 	}
 }
 
@@ -30,7 +36,7 @@ function loadHouse () {
 		var room = $('<div />', { 'class': 'center-child house-' + o });
 
 		if(o !== 'balcony'){
-			room.click(function(){ loadMain('view-' + o); });
+			room.click(function(){ navigateMain('view-' + o); });
 		}
 		room.append(o).appendTo(house);
 	});
@@ -38,29 +44,48 @@ function loadHouse () {
 	return ssr.append(ssrC.append(house));
 }
 
-function loadDefault (i) {
-	var ssr = $('<div />', { 'class': 'static-size-ratio ssr-80'});
-	var ssrC = $('<div />', { 'class': 'ssr-content center-child'});
-	var room = $('<div />', { 'class': 'center-child room-' + i.substr(5) });
-
-	return ssr.append(ssrC.append(room.append(i.substr(5))));
-}
-
-function loadOptions (j) {
+function loadOptions (page) {
 	var ul = $('<ul />', { 'class': 'no-bullet options-list'});
 
-	$.each(settings[j], function(i, o){
+	$.each(appSettings[page], function(i, o){
 		var li = $('<li />', { 'class': 'options-list-item' });
-		var div = $('<div />', { 'class': 'switch tiny' });
-		var input = $('<input />', {
-			'type': 'checkbox',
-			'id': i,
-			'checked': o.value === 1 }).click(function () {
-				settings[j][this.id]["value"] = 
-				+(this.checked);
+		li.append(i);
+
+		switch (o.type) {
+		case 'switch':
+			var div = $('<div />', { 'class': 'switch tiny' });
+			var input = $('<input />', {
+				'type': 'checkbox',
+				'id': i,
+				'checked': o.value === 1 
+			}).click(function () {
+				o.value = +(this.checked);
 			});
-		var label = $('<label />', { 'for': i });
-		li.append(i, div.append(input, label)).appendTo(ul);
+			var label = $('<label />', { 'for': i });
+			li.append(div.append(input, label)).appendTo(ul);
+			break;
+		case 'slider':
+			var div = $('<div />', {
+				'id': 'slider-' + i,
+				'class': 'range-slider',
+				'data-slider': o.value,
+				'data-options': 'start: ' + o.start + '; end: ' + o.end + 
+					'; display_selector: #' + i + ';'
+			}).on('change.fndtn.slider', function(){
+				o.value = $(this).attr('data-slider');
+			});
+			var span1 = $('<span />', {
+				'class': 'range-slider-handle',
+				'role': 'slider',
+				'tabindex': '0'
+			});
+			var span2 = $('<span />', { 'class': 'range-slider-active-segment' });
+			var span3 = $('<span />', { 'id': i });
+			var input = $('<input>', { 'type': 'hidden' });
+			li.append(' ', span3, '&deg;c', div.append(span1, span2, input)).appendTo(ul);
+			break;
+		}
 	});
 	return ul;
 }
+
--- smart-hem/js/settings.js
+++ smart-hem/js/settings.js
@@ -1,45 +1,58 @@
-var settings = {
-	"view-house": {
-		"lock": {
-			"value": 1
+var appSettings = {
+	'view-house': {
+		'lock': {
+			'value': 1,
+			'type': 'switch'
 		},
-		"alarm": {
-			"value": 0
+		'alarm': {
+			'value': 0,
+			'type': 'switch'
 		},
-		"light": {
-			"value": 1
+		'light': {
+			'value': 1,
+			'type': 'switch'
 		},
-		"temperature": {
-			"value": 21
+		'temperature': {
+			'value': '21',
+			'type': 'slider',
+			'start': 15,
+			'end': 25
 		},
-		"cleaning-robot": {
-			"value": 1
+		'cleaning-robot': {
+			'value': 1,
+			'type': 'switch'
 		},
-		"music": {
-			"value": 0
+		'music': {
+			'value': 0,
+			'type': 'switch'
 		}
 	},
-	"view-livingroom": {
-		"lightl": {
-			"value": 1
+	'view-livingroom': {
+		'light': {
+			'value': 1,
+			'type': 'switch'
 		}
 	},
-	"view-bedroom": {
-		"lightb": {
-			"value": 1
+	'view-bedroom': {
+		'light': {
+			'value': 1,
+			'type': 'switch'
 		},
-		"bed-vibration": {
-			"value": 1
+		'bed-vibration': {
+			'value': 1,
+			'type': 'switch'
 		}
 	},
-	"view-kitchen": {
-		"lightk": {
-			"value": 1
+	'view-kitchen': {
+		'light': {
+			'value': 1,
+			'type': 'switch'
 		}
 	},
-	"view-wc": {
-		"lightwc": {
-			"value": 0
+	'view-wc': {
+		'light': {
+			'value': 0,
+			'type': 'switch'
 		}
 	}
 };
